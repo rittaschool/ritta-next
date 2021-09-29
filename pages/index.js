163891996a621ea13b5c4e12d9896ed78ca4408e
raikasdev <roni.aikas@gmail.com>
2021-09-29 21:33:43 +0300

    Add redirect

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,13 @@ import AdminLayout from '../components/Layout/AdminLayout';
 import React from 'react';
 
 export default class Index extends React.Component {
-    render() {
+  componentDidMount() {
+    window.location.href = '/auth';
+  }  
+  
+  render() {
         return <AdminLayout contentTitle={'Home'} contentTitleButton={<i className="fa fa-2x fa-home"/>} url={this.props.url}>
-          <h1>Shesh</h1>
+          <h1>Redirecting to login</h1>
           <hr />
         </AdminLayout>
     }
