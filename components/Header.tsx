2bcba9c9722238c73202db4f725a5f32f23c5fd8
kymppi <me@midka.dev>
2022-02-08 19:02:07 +0200

    fix: replace header sticky to fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import logoImage from '../assets/Ritta_White.svg';
 
 const Header = () => {
   return (
-    <header className="w-full flex sticky top-0 left-0 h-20 bg-primary items-center">
+    <header className="w-full flex fixed top-0 left-0 h-20 bg-primary items-center">
       <div className="relative h-full w-36 m-4">
         <Image layout="fill" src={logoImage} alt="Ritta Logo" />
       </div>
