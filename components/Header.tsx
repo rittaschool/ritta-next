92d7bc20bae9d82578b2259f92f9dea59aacc4d9
kymppi <me@midka.dev>
2022-02-08 18:55:32 +0200

    fix: replace div tags with header for better seo

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,11 +4,11 @@ import logoImage from '../assets/Ritta_White.svg';
 
 const Header = () => {
   return (
-    <div className="w-full flex sticky top-0 left-0 h-20 bg-primary items-center">
+    <header className="w-full flex sticky top-0 left-0 h-20 bg-primary items-center">
       <div className="relative h-full w-36 m-4">
         <Image layout="fill" src={logoImage} alt="Ritta Logo" />
       </div>
-    </div>
+    </header>
   );
 };
 
