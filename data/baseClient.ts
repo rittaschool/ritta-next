86178e89275a9c992fc295f7d147b55aef95b19c
Midka <me@midka.dev>
2022-03-20 15:06:50 +0200

    fido2 stuff

--- data/baseClient.ts
+++ data/baseClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const graphqlClient = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri: 'https://devapp.midka.dev/graphql',
   cache: new InMemoryCache({ addTypename: false }),
 });
--- pages/login.tsx
+++ pages/login.tsx
@@ -3,8 +3,14 @@ import { AuthenticationScreen, Button, Card, Form } from '../components';
 import useAuthentication from '../hooks/useAuthentication';
 
 const Login = () => {
-  const { startLoginProcess, submitPassword, user, authenticated, challenge } =
-    useAuthentication();
+  const {
+    startLoginProcess,
+    submitPassword,
+    user,
+    authenticated,
+    challenge,
+    fido2,
+  } = useAuthentication();
   const [currentScreen, setCurrentScreen] = useState('identifier');
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
@@ -25,6 +31,8 @@ const Login = () => {
     } else if (currentScreen === 'password') {
       const password: string = (passwordRef.current as any).value;
 
+      console.log('pass', password);
+
       if (!challenge) return console.log('No challenge');
 
       const result = await submitPassword(password, challenge);
@@ -32,48 +40,59 @@ const Login = () => {
     }
   };
 
+  const Webauthn = async () => {
+    const result = await fido2.startSetup('me@midka.dev');
+
+    if (result == null) return;
+
+    const result2 = await fido2.finishSetup(result!);
+
+    console.log(result2);
+  };
+
   return (
     <div className="flex h-screen items-center justify-center bg-primary">
       <div className="max-w-fit">
-        <Card
+        {/* <Card
           isImageAtTop
           imageSrc="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        >
-          <Form onSubmit={handleSubmit}>
-            <div className="h-full min-w-[360px] px-12 pb-12 flex flex-col">
-              {currentScreen === 'identifier' && (
-                <AuthenticationScreen title="Kirjaudu Sisään">
-                  <input
-                    ref={emailRef}
-                    type="email"
-                    className="rounded text-primary mb-4 mt-8 w-full max-w-md border-light"
-                    name="identifier"
-                    placeholder="Käyttäjänimi"
-                  />
-                  <Button
-                    text="Seuraava"
-                    className="rounded bg-blue-500 p-2 w-full max-w-md text-light"
-                  />
-                </AuthenticationScreen>
-              )}
-              {currentScreen === 'password' && (
-                <AuthenticationScreen title="Syötä Salasana">
-                  <input
-                    ref={passwordRef}
-                    type="password"
-                    className="rounded text-primary mb-4 mt-8 w-full max-w-md border-light"
-                    name="password"
-                    placeholder="Salasana"
-                  />
-                  <Button
-                    text="Seuraava"
-                    className="rounded bg-blue-500 p-2 w-full max-w-md text-light"
-                  />
-                </AuthenticationScreen>
-              )}
-            </div>
-          </Form>
-        </Card>
+        > */}
+        <Button text="WebAuthN" onClick={() => Webauthn()} />
+        <Form onSubmit={handleSubmit}>
+          <div className="h-full min-w-[360px] px-12 pb-12 flex flex-col">
+            {currentScreen === 'identifier' && (
+              <AuthenticationScreen title="Kirjaudu Sisään">
+                <input
+                  ref={emailRef}
+                  type="email"
+                  className="rounded text-primary mb-4 mt-8 w-full max-w-md border-light"
+                  name="identifier"
+                  placeholder="Käyttäjänimi"
+                />
+                <Button
+                  text="Seuraava"
+                  className="rounded bg-blue-500 p-2 w-full max-w-md text-light"
+                />
+              </AuthenticationScreen>
+            )}
+            {currentScreen === 'password' && (
+              <AuthenticationScreen title="Syötä Salasana">
+                <input
+                  ref={passwordRef}
+                  type="password"
+                  className="rounded text-primary mb-4 mt-8 w-full max-w-md border-light"
+                  name="password"
+                  placeholder="Salasana"
+                />
+                <Button
+                  text="Seuraava"
+                  className="rounded bg-blue-500 p-2 w-full max-w-md text-light"
+                />
+              </AuthenticationScreen>
+            )}
+          </div>
+        </Form>
+        {/* </Card> */}
       </div>
     </div>
   );
